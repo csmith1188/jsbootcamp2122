ed737e2af2a3a7b957126e28d597e07edc5f7ac1
JacobSmith2005 <90261539+JacobSmith2005@users.noreply.github.com>
2021-12-01 11:33:09 -0500

    ye

---
+++ jsmith/day 2/day2.js
@@ -0,0 +1,90 @@
+//import express js modules
+const express = require('express');
+//create a express app
+const app = express()
+//create one character object
+var character  = {
+  name:'glob',
+  race: 'goblin',
+  profession: 'mercenary',
+  equipment: {
+    head: {},
+    chest: {},
+    legs: {},
+    arm_p: {},
+    arm_s: {},
+  },
+  inventory: [],
+  abilities: [],
+  stats: {
+    attack: '∞',
+    defense: '∞',
+    hp_current: '∞',
+    hp_max: '∞',
+  },
+  //this method searches a item and adds it to inventory
+  //And adds it to this character's Inventory
+  pickupitem: function(searchName) {
+    for (var item of item_list) {
+      if (item.name == searchName) {
+        console.log("founda match");
+        this.inventory.push(item);
+        break;
+        }
+      }
+    },
+    //this method searches for a certain slot
+    //it then removes it
+    unequipitem: function(slot) {
+      for (var slotName in this.equipment) {
+        console.log(slotName);
+        if (slotName == slot) {
+          console.log("found da slot, removing");
+          this.equipment.slotName = {};
+          break;
+        }
+      } {
+
+      }
+    }
+}
+//holds all possible items
+var item_list = [
+  {
+    name: 'goblin blade',
+    slot: `arm_p`,
+    bonuses: {
+      attack: 5
+    }
+  },
+  {
+    name: 'goblin shield',
+    slot: `arm_s`,
+    bonuses: {
+      defense: 5
+    }
+  }
+];
+
+//create a get endpoint
+app.get('/', (req,res) => {
+  character.pickupitem('goblin blade');
+  character.unequipitem('arm_p')
+  res.send(
+    `
+    <p>name: ${character.name}
+    <p>race: ${character.race}
+    <p>profession: ${character.profession}
+    <p>equipment: ${character.equipment}
+    <p>inventory: ${character.inventory}
+    <p>abilities: ${character.abilities}
+    <p>Attack: ${character.stats.attack}</p>
+    <p>Defence: ${character.stats.defense}</p
+    <p>Current HP: ${character.stats.hp_current}</p>
+    <p>Max HP: ${character.stats.hp_max}</p>}
+    <p>Inventory: ${JSON.stringify(character.inventory)}</p>
+    `
+  );
+});
+//create a app listen
+app.listen(3000)
