bf0c21ce6603de4bc3d3b44a31a3fdb731c22692
Mellen <71459733+Jenna2023251@users.noreply.github.com>
2021-12-03 11:06:37 -0500

    day4

--- jmellen/day3/day3.js
+++ jmellen/day3/day3.js
@@ -1,16 +1,15 @@
-// Day 2 JS Bootcamp
-// by Jenna
-
-// Import expressJS module
+//Import expressJS module
 const express = require('express');
 
-// Create an express application object
+// Create an epxress application object
 const app = express()
 
-// Create a character object
+app.set("view engine", "ejs");
+
+// Create one character object
 var char = {
   name: 'Hatsune Miku',
-  race: 'Human', // coping
+  race: 'Human',
   profession: 'Virtual Idol',
   equipment: {
     head: {},
@@ -27,33 +26,36 @@ var char = {
     hp_current: '∞',
     hp_max: '∞'
   },
-  // This method searched for an item in the item list with this name and adds it to this character's inventory
+  //This method searches for an item in the itme list with this name
+  //And adds it to this character's inventory
   pickupItem: function(searchName) {
+    console.log(this);
     for (var item of item_list) {
       console.log(item.name);
       if (item.name == searchName) {
-        console.log('poggie woggies');
+        console.log("poggie woggies");
         this.inventory.push(item);
         break;
-      };
+      }
     }
   },
-  // This method searches for a given slot and overwrites it with an empty object
+  //This method searches for a given slot and overwrites
+  //it with an empty object
   unequipItem: function(slot) {
     for (var slotName in this.equipment) {
       console.log(slotName);
       if (slotName == slot) {
-        console.log('Removing - not so poggie woggies');
+        console.log("Removing - not so poggie woggies");
         this.equipment.slotName = {};
         break;
       }
     }
-
   }
 }
 
 // This holds all possible items
-var item_list = [{
+var item_list = [
+  {
     name: 'Diamond Pickaxe',
     slot: 'arm_p',
     bonuses: {
@@ -73,17 +75,8 @@ var item_list = [{
 app.get('/', (req, res) => {
   char.pickupItem('Diamond Pickaxe');
   char.unequipItem('arm_p');
-  res.send(`
-    <h1>こんにちは</h1>
-    <p>Name: ${char.name} </p>
-    <p>Race: ${char.race} </p>
-    <p>Profession: ${char.profession} </p>
-    <p>Attack: ${char.stats.attack} </p>
-    <p>Defense: ${char.stats.defense} </p>
-    <p>HP: ${char.stats.hp_current} / ${char.stats.hp_max} </p>
-    <p>Inventory: ${JSON.stringify(char.inventory)}</p>
-    `);
+  res.render('profile', { sendData: char })
 });
 
-// Start an HTTP listen server
-app.listen(3000)
+//Start an http listen server
+app.listen(3000);
